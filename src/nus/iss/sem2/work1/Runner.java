8a0eb4dccb1c151473c516c6de3ed495edb5fb5f
c.banisetty <c.banisetty@WIN-IF24F14376.mshome.net>
2018-03-06 00:55:20 +0800

    variables

--- src/nus/iss/sem2/work1/Runner.java
+++ src/nus/iss/sem2/work1/Runner.java
@@ -11,8 +11,21 @@ public class Runner {
 
 	public void PrintMessage() {
 		System.out.println("Welcome to my first Program");
+		System.out.println(intvariable);
+		System.out.println(shortvariable);
+		System.out.println(longvariable);
+		System.out.println(bytevariable);
+		System.out.println(floatvariable);
+		System.out.println(doublevariable);
+		System.out.println(boolvariable);
 	}
-	
+	int intvariable =20;
+	short shortvariable = 20;
+	long longvariable=20L;
+	byte bytevariable=20;
+	float floatvariable=0.56565f;
+	double doublevariable=3456.1232d;
+	boolean boolvariable=true;
 	/**
 	 * @param args
 	 */
