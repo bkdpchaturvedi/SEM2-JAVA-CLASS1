1b2508dbb10d8c67479390691daf9c8021264e7b
c.banisetty <c.banisetty@WIN-IF24F14376.mshome.net>
2018-03-06 00:35:06 +0800

    My first Program

---
+++ src/nus/iss/sem2/work1/Runner.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package nus.iss.sem2.work1;
+
+/**
+ * @author c.banisetty
+ *
+ */
+public class Runner {
+
+	public void PrintMessage() {
+		System.out.println("Welcome to my first Program");
+	}
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Runner run=new Runner();
+		run.PrintMessage();
+	}
+
+}
